cadcf0d63b2594f0d40381bdace9815cd68cedce
Emir <emirkan.ulker9@gmail.com>
2023-12-25 13:31:27 +0300

    v

---
+++ denemelerim/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace denemelerim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mskNumara = new System.Windows.Forms.MaskedTextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numara:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad Soyad:";
+            // 
+            // mskNumara
+            // 
+            this.mskNumara.Location = new System.Drawing.Point(148, 32);
+            this.mskNumara.Mask = "0000";
+            this.mskNumara.Name = "mskNumara";
+            this.mskNumara.Size = new System.Drawing.Size(100, 26);
+            this.mskNumara.TabIndex = 2;
+            this.mskNumara.ValidatingType = typeof(int);
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(148, 84);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 26);
+            this.txtAdSoyad.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 215);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.mskNumara);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox mskNumara;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ denemelerim/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+
+namespace denemelerim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-BJO2DGU\\SQLEXPRESS;Initial Catalog=DersVeriTabani;Integrated Security=True");
+
+
+        bool durum;
+        void mukerrer()
+        {
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("select * from Tbl_Bilgi where Numara=@p3",baglanti);
+            komut2.Parameters.AddWithValue("@p3",mskNumara.Text);
+            SqlDataReader dr = komut2.ExecuteReader();
+            if (dr.Read())
+            {
+                durum = false;
+            }
+            else
+            {
+                durum = true;
+            }
+            baglanti.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mukerrer();
+            if (durum == true) 
+            { 
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into Tbl_Bilgi (Numara,AdSoyad) values (@p1,@p2)",baglanti);
+            komut.Parameters.AddWithValue("@p1",mskNumara.Text);
+            komut.Parameters.AddWithValue("@p2",txtAdSoyad.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kayıt Eklendi");
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Numarası Zaten Mevcut", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
